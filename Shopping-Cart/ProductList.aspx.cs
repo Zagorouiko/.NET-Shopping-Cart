ba1c89661d5128012920ee5fd64997e3ab96f658
Dr.Z <Dr.Z@DRZ-PC>
2016-12-21 16:03:51 -0800

    Added to product list and data binded products.

--- Shopping-Cart/ProductList.aspx.cs
+++ Shopping-Cart/ProductList.aspx.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Shopping_Cart.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Shopping_Cart.Models;
+using System.Web.ModelBinding;
 
 namespace Shopping_Cart
 {
@@ -12,6 +15,17 @@ namespace Shopping_Cart
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetProducts([QueryString("id")] int? categoryId)
+        {
+            var _db = new Shopping_Cart.Models.ProductContext();
+            IQueryable<Product> query = _db.Products;
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+            return query;
         }
     }
 }
